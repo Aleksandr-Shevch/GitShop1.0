2c202a24b4e482a3dcb251c1612f274991f39ca6
Aleksandr-Shevch <waidyxa@gmail.com>
2022-08-13 16:15:24 +0300

    Add validator

---
+++ src/utils/Validator.java
@@ -0,0 +1,60 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Validator {
+
+    public static String validateName(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.print("Пусто! Введите имя клиента: ");
+            str = scanner.nextLine().trim();
+        }
+
+        return str;
+    }
+
+
+    public static int validateQuantityInput(Scanner scanner) {
+
+        int quantity;
+        String str1 = null;
+
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!%nВведите количество!: ", str);
+        }
+
+        quantity = scanner.nextInt();
+
+        while (quantity <= 0) {
+            System.out.println("Неверное значение! Введите количество: ");
+
+            quantity = scanner.nextInt();
+        }
+        return quantity;
+    }
+
+
+    public static double validatePriceInput(Scanner scanner) {
+
+        double price;
+        String str1 = null;
+
+        while (!scanner.hasNextDouble()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!%nВведите цену!: ", str);
+        }
+
+        price = scanner.nextDouble();
+
+        while (price <= 0) {
+            System.out.print("Неверное значение! Введите цену: ");
+
+            price = scanner.nextDouble();
+        }
+        return price;
+    }
+}
+
+
